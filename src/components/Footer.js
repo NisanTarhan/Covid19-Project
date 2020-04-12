86db3703becaf2258457e942f810e2952e375caa
Nisan Ercan Tarhan <nisan_tarhan@hotmail.com>
2020-04-12 11:53:37 +0300

    Header and Footer fontsize are fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,15 @@ import { primary, secondary } from '../res/colors'
 import { font } from '../res/fonts';
 
 const FooterLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   grid-column: 1/-1;
   background: ${props => props.primary.bg};
   color: ${props => props.secondary.text};
   text-align: center;
   font-family: ${props => props.font.secondary};
+  font-size: 0.7vw;
 `
 
 const Footer = () => {
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,15 @@ import { primary, secondary } from '../res/colors'
 import { font } from '../res/fonts';
 
 const HeaderLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   grid-column: 1/-1;
   background: ${props => props.primary.bg};
   color: ${props => props.secondary.text};
   text-align: center;
   font-family: ${props => props.font.secondary};
+  font-size: 0.8vw;
 `
 
 const Header = () => {
